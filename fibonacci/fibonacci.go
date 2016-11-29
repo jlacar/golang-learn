24584ddcf5e7c172f1587f1e9f694d3c97e62f7c
Junilu Lacar <jlacar@cisco.com>
2016-11-29 17:51:53 -0500

    Extended fibonacci to further demo closure; formatted sieve code

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
-// fib is a closure that generates the fibonacci series
-func fib() func() int {
-	var fib0, fib1 = 0, 1
-	return func() (f int) {
+var n int
+
+// fib returns a closure that generates the fibonacci series
+func fib() func() uint64 {
+	var fib0, fib1 uint64 = 0, 1
+	return func() (f uint64) {
 		f, fib0, fib1 = fib0, fib1, fib0+fib1
 		return
 	}
 }
 
-func main() {
-	f := fib()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %v [-n]\n\n"+
+			"Options:\n\n", os.Args[0])
+		flag.PrintDefaults()
 	}
+
+	flag.IntVar(&n, "n", 10, "print first `N` numbers of the Fibonacci series")
+	flag.Parse()
+}
+
+func printSeries(heading string, times int, fn func() uint64) {
+	fmt.Printf("\n%v:\n", heading)
+	for i := 0; i < times; i++ {
+		fmt.Println(fn())
+	}
+}
+
+func main() {
+	f, g := fib(), fib()
+
+	printSeries("First series", n, f)
+	printSeries("Second series", n+1, g)
+	printSeries("Continue first series", n, f)
+	printSeries("Continue second series", n, g)
 }
--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,4 +1,4 @@
-// An of The Sieve of Eratosthenes
+// A Go implementation of The Sieve of Eratosthenes
 package main
 
 import (
@@ -10,39 +10,39 @@ import (
 var primes []bool
 
 func findPrimes(max int) {
-    primes = make([]bool, max + 1)
-    
-    for i := 2; i < len(primes); i++ {
-       primes[i] = true
-    }
-    
-    for i := 2; i < len(primes); i++ {
-       if primes[i] {
-          for j := 2*i; j < len(primes); j += i {
-             primes[j] = false
-          }
-       }
-    }    
+	primes = make([]bool, max+1)
+
+	for i := 2; i < len(primes); i++ {
+		primes[i] = true
+	}
+
+	for i := 2; i < len(primes); i++ {
+		if primes[i] {
+			for j := 2 * i; j < len(primes); j += i {
+				primes[j] = false
+			}
+		}
+	}
 }
 
 func listPrimes() {
-    count := 0
-    for i := 0; i < len(primes); i++ {
-       if primes[i] {
-          fmt.Printf("%4v, ", i)
-          count++
-          if count == 20 {
-             fmt.Print("\n")
-             count = 0
-          }
-       }    
-    }
-    fmt.Print("\n")
+	count := 0
+	for i := 0; i < len(primes); i++ {
+		if primes[i] {
+			fmt.Printf("%4v, ", i)
+			count++
+			if count == 20 {
+				fmt.Print("\n")
+				count = 0
+			}
+		}
+	}
+	fmt.Print("\n")
 }
 
 func main() {
-    max, _ := strconv.Atoi(os.Args[1])
-    
-    findPrimes(max)
-    listPrimes()
+	max, _ := strconv.Atoi(os.Args[1])
+
+	findPrimes(max)
+	listPrimes()
 }
